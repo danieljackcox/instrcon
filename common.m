87814f507024fe0d2731bc8dc7197c2def318e14
Daniel Cox <daniel+github@danielcox.fi>
2016-12-16 20:34:16 +0200

    better commenting

--- common.m
+++ common.m
@@ -4,14 +4,14 @@
 % and measurement
 % OPEN : Opens a GPIB device, identifies and returns device object
 classdef common < handle
-    
+
     properties
         verb = 0; % verbosity, off by default
         addr; % address of device
         vend; % board vendor, by default 'ni'
         bus; % bus number, by default 0
     end
-    
+
     methods
         % constructor object
         function obj = common
@@ -19,81 +19,100 @@ classdef common < handle
             % of the drivers folder
             obj;
         end
-        
+
     end
-    
+
     methods
-        
+
         % open function, will open gpib device, identify it and return correct
         % device driver object
+		% vend and bus are optional arguments
         function handle = open(obj, addr, vend, bus)
-            % vend and bus are optional arguments
-            
+
+            % if no address is given then show an error
             if(~exist('addr', 'var'))
-                % if no address is given then show an error
                 error('Address must be given');
             end
-            
+
+
+			% if no board is given then set a default here
             if(~exist('vend', 'var'))
-                % if no board is given then set a default here
                 vend = 'ni';
             end
-            
-            
+
+
+			% is no bus is given then set default here
             if(~exist('bus', 'var'))
-                % is no bus is given then set default here
                 bus = 0;
             end
-            
-            % first check that the variables passed are ok
+
+
+            %%%
+            % now do some sanity checks on variables that have been passed
+			%%%
+
+
+			% check if the vendor passed is a character array
             if(~ischar(vend))
-                % check if the vendor passed is a character array
                 error('Vendor id should be a character array (addr: %s)', num2str(addr));
             end
+
+			% make sure bus is a number
             if(~isnumeric(bus))
-                % make sure bus is a number
                 error('Bus should be a number (addr: %s)', num2str(addr));
             end
+
+			% make sure address is a number
             if(~isnumeric(addr))
-                % make sure address is a number
                 error('Address should be a number');
             end
-            
+
+			% matlab only supports four vendor types, so we make sure that
+			% the type passed is one of those
             if( ~(strcmpi(vend, 'ni')||strcmpi(vend, 'agilent')||strcmpi(vend, 'ics')||strcmpi(vend, 'mcc')) )
-                % make sure the vendor is of a type recognised by matlab
                 error('Unrecognised vendor type (addr: %s)', num2str(addr));
             end
-            
-            % create the gpib object and then open it
+
+
+
+            %%%
+			% GPIB object creation and opening
+			%%%
+
+
+			% create a gpib object with the passed variables (or defaults)
+			% and assign it to a temporary variable so we can open it
             try
                 instr = gpib(vend, bus, addr);
                 fopen(instr);
             catch
                 error('Cannot open GPIB device at address %s', num2str(addr));
             end
-            
+
+			% assuming everything went well we can then
             % flush input and output buffers, this is important on certain devices
             % such as SR830
             flushinput(instr); %software buffers
             flushoutput(instr);
             clrdevice(instr); % hardware buffers
-            
+
             % print a confirmation message if the option is chosen
             if( obj.verb == 1 )
                 fprintf(1, 'Device opened at %s (GPIB)', num2str(ADDR));
             end
-            
+
             %query device for its identity
             identity = query(instr, '*IDN?');
-            
+
+			% UNFINISHED, only supports SR830
             if(~isempty(strfind(identity, 'Stanford_Research_Systems,SR830')))
                 handle = SR830;
                 handle.instr = instr;
             end
-            
-            
+
+
         end
-        
+
     end
-    
+
 end
