526f8ee5aacb99abb9890b0602efee44345ca0e9
Nano <Nano@130.233.174.118>
2016-12-16 16:42:08 +0200

    removed seperate get/set functionality and rolled into single functions
    added a lot more SR830 functions

--- common.m
+++ common.m
@@ -4,93 +4,96 @@
 % and measurement
 % OPEN : Opens a GPIB device, identifies and returns device object
 classdef common < handle
-
-properties
-verb = 0; % verbosity, off by default
-addr; % address of device
-vend; % board vendor, by default 'ni'
-bus; % bus number, by default 0
-end
-
-methods
-% constructor object
-function obj = common
-	obj;
-end
-
-end
-
-methods
-
-% open function, will open gpib device, identify it and return correct
-% device driver object
-function handle = open(obj, addr, vend, bus)
-	% BOARD and BUS are optional arguments
-
-	if(~exist('addr', 'var'))
-		% if no address is given then show an error
-		error('Address must be given');
-	end
-
-	if(~exist('vend', 'var'))
-		% if no board is given then set a default here
-		vend = 'ni';
-	end
-
-	if(~exist('bus', 'var'))
-		% is no bus is given then set default here
-		bus = 0;
-	end
-
-	% first check that the variables passed are ok
-	if(~ischar(vend))
-		% check if the vendor passed is a character array
-		error('Vendor id should be a character array (addr: %s)', num2str(addr));
-	end
-	if(~isnumeric(bus))
-		% make sure bus is a number
-		error('Bus should be a number (addr: %s)', num2str(addr));
-	end
-	if(~isnumeric(addr))
-		% make sure address is a number
-		error('Address should be a number');
-	end
-
-	if( ~strcmpi(vend, 'ni')||~strcmpi(vend, 'agilent')||~strcmpi(vend, 'ics')||~strcmpi(vend, 'mcc') )
-		% make sure the vendor is of a type recognised by matlab
-		error('Unrecognised vendor type (addr: %s)', num2str(addr));
-	end
-
-	% create the gpib object and then open it
-	try
-		instr = gpib(vend, bus, addr);
-		fopen(instr);
-	catch
-		error('Cannot open GPIB device at address %s', num2str(addr));
-	end
-
-	% flush input and output buffers, this is important on certain devices
-	% such as SR830
-	flushinput(instr); %software buffers
-	flushoutput(instr);
-	clrdevice(instr); % hardware buffers
-
-	% print a confirmation message if the option is chosen
-	if( verb == 1 )
-		fprintf(1, 'Device opened at %s (GPIB)', num2str(ADDR));
-	end
-
-	%query device for its identity
-	identity = query(instr, '*IDN?');
-
-	if(~isempty(strfind(identity, 'Stanford_Research_Systems,SR830')))
-		handle = drivers/SR830;
-		handle.instr = instr;
-	end
-
-
-end
-
-end
-
+    
+    properties
+        verb = 0; % verbosity, off by default
+        addr; % address of device
+        vend; % board vendor, by default 'ni'
+        bus; % bus number, by default 0
+    end
+    
+    methods
+        % constructor object
+        function obj = common
+            addpath('drivers'); % make sure matlab can see the contents
+            % of the drivers folder
+            obj;
+        end
+        
+    end
+    
+    methods
+        
+        % open function, will open gpib device, identify it and return correct
+        % device driver object
+        function handle = open(obj, addr, vend, bus)
+            % vend and bus are optional arguments
+            
+            if(~exist('addr', 'var'))
+                % if no address is given then show an error
+                error('Address must be given');
+            end
+            
+            if(~exist('vend', 'var'))
+                % if no board is given then set a default here
+                vend = 'ni';
+            end
+            
+            
+            if(~exist('bus', 'var'))
+                % is no bus is given then set default here
+                bus = 0;
+            end
+            
+            % first check that the variables passed are ok
+            if(~ischar(vend))
+                % check if the vendor passed is a character array
+                error('Vendor id should be a character array (addr: %s)', num2str(addr));
+            end
+            if(~isnumeric(bus))
+                % make sure bus is a number
+                error('Bus should be a number (addr: %s)', num2str(addr));
+            end
+            if(~isnumeric(addr))
+                % make sure address is a number
+                error('Address should be a number');
+            end
+            
+            if( ~(strcmpi(vend, 'ni')||strcmpi(vend, 'agilent')||strcmpi(vend, 'ics')||strcmpi(vend, 'mcc')) )
+                % make sure the vendor is of a type recognised by matlab
+                error('Unrecognised vendor type (addr: %s)', num2str(addr));
+            end
+            
+            % create the gpib object and then open it
+            try
+                instr = gpib(vend, bus, addr);
+                fopen(instr);
+            catch
+                error('Cannot open GPIB device at address %s', num2str(addr));
+            end
+            
+            % flush input and output buffers, this is important on certain devices
+            % such as SR830
+            flushinput(instr); %software buffers
+            flushoutput(instr);
+            clrdevice(instr); % hardware buffers
+            
+            % print a confirmation message if the option is chosen
+            if( obj.verb == 1 )
+                fprintf(1, 'Device opened at %s (GPIB)', num2str(ADDR));
+            end
+            
+            %query device for its identity
+            identity = query(instr, '*IDN?');
+            
+            if(~isempty(strfind(identity, 'Stanford_Research_Systems,SR830')))
+                handle = SR830;
+                handle.instr = instr;
+            end
+            
+            
+        end
+        
+    end
+    
 end
--- drivers/SR830.m
+++ drivers/SR830.m
@@ -19,15 +19,9 @@ classdef SR830 < handle	%generate new class for SRS830 and make it a subclass of
             %nothing
         end
         
-        function setvoltage(obj, V, channel)
-            if(nargin == 1) %only object is being passed, return an error
-                error('You must pass a voltage');
-            end
+        function volts = voltage(obj, V, channel)
             
-            if(~isnumeric(V))
-                error('Voltage must be a number');
-            end
-            
+            % check that the channel variable is ok first
             if(exist('channel', 'var'))
                 if(~isnumeric(channel))
                     error('Channel must be an integer number');
@@ -40,29 +34,187 @@ classdef SR830 < handle	%generate new class for SRS830 and make it a subclass of
                 error('Channel number must be between 1 and 4');
             end
             
-            fprintf(obj.instr, sprintf('AUX V %d, %f', channel, V));
+            % if nothing or only channel is being passed then return the
+            % voltage set
+            if(nargin == 1 || ~exist('V', 'var') || isempty(V))
+                fprintf(obj.instr, sprintf('AUXV? %d', channel));
+                volts = fscanf(obj.instr, '%f');
+            else
+                
+                if(~isnumeric(V))
+                    error('Voltage must be a number');
+                end
+                
+                fprintf(obj.instr, sprintf('AUX V %d, %f', channel, V));
+                
+            end
         end
         
-        function voltage = getvoltage(obj, channel)
-            
-            if(exist('channel', 'var'))
-                if(~isnumeric(channel))
-                    error('Channel must be an integer number');
-                end
+        
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % ref: sets the device to use internal or external    %
+        % frequency reference or queries to get the ref       %
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        function output = ref(obj, ref)
+            if( nargin == 1 || isempty(ref) )
+                fprintf(obj.instr, 'FMOD?');
+                output = fscanf(obj.instr, '%d');
             else
-                channel = 1;
+                
+                if( ~isnumeric(ref))
+                    error('Provided reference must be an integer or logical');
+                end
+                
+                % passes all error checking, then execute
+                fprintf(obj.instr, 'FMOD %d', ref);
+                
             end
-            
-            if(channel < 1 || channel > 4)
-                error('Channel number must be between 1 and 4');
-            end
-            
-            
-            fprintf(obj.instr, 'AUXV? %d', channel);
-            voltage = fscanf(obj.instr, '%f');
             
         end
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % freq: sets or reads internal frequency              %
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+        function output = freq(obj, freq)
+            if( nargin == 1 || isempty(freq) )
+                fprintf(obj.instr, 'FREQ?');
+                output = fscanf(obj.instr, '%f');
+            else
+                
+                if( ~isnumeric(freq))
+                    error('Provided frequency must be a real number');
+                end
+                
+                fprintf(obj.instr, 'FREQ %f', freq);
+                
+            end
             
+        end
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % reftrig: sets the reference trigger to sine zero crossing       %
+        % , TTL rising edge or TTL falling edge, also queries for current %
+        % setting                                                         %
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+        function output = reftrig(obj, trigtype)
+            if( nargin == 1 || isempty(trigtype) )
+                fprintf(obj.instr, 'RSLP?');
+                output = fscanf(obj.instr, '%d');
+            else
+                
+                if( ~isnumeric(trigtype))
+                    error('Provided reference trigger must be an integer between 0 and 2');
+                end
+                
+                if( ~(isequal(trigtype, 0) || isequal(trigtype, 1) || isequal(trigtype, 2)) )
+                    error('Input must be 0, 1, or 2');
+                end
+                
+                fprintf(obj.instr, 'RSLP %d', trigtype);
+                
+            end
+            
+        end
+        
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % harmonic: sets or returns the measurement harmonic           %
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+        function output = harmonic(obj, harmonic)
+            if( nargin == 1 || isempty(harmonic) )
+                fprintf(obj.instr, 'HARM?');
+                output = fscanf(obj.instr, '%d');
+            else
+                
+                if( ~isnumeric(harmonic))
+                    error('Harmonic must be an integer larger than 1');
+                end
+                
+                
+                fprintf(obj.instr, 'HARM %d', harmonic);
+                
+            end
+            
+        end
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % excitation: sets or returns the AC output sine wave voltage (in RMS) %
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+        function output = excitation(obj, excitation)
+            if( nargin == 1 || isempty(excitation) )
+                fprintf(obj.instr, 'SLVL?');
+                output = fscanf(obj.instr, '%f');
+            else
+                
+                if( ~isnumeric(excitation))
+                    error('AC Sine Excitation must be a number');
+                end
+                
+                
+                fprintf(obj.instr, 'SLVL %f', excitation);
+                
+            end
+            
+        end
+        
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % inputconfig: sets or reads the device input configuration, choose  %
+        % between 0 (A), 1 (A-B), 2 (I 1MOhm), 3 (100 MOhm)                  %
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+        function output = inputconfig(obj, inputconfig)
+            if( nargin == 1 || isempty(inputconfig) )
+                fprintf(obj.instr, 'ISRC?');
+                output = fscanf(obj.instr, '%d');
+            else
+                
+                if( ~isnumeric(inputconfig))
+                    error('Input Configuration must be a number');
+                end
+                
+                if( ~(isequal(inputconfig, 0) || isequal(inputconfig, 1) || isequal(inputconfig, 2) || isequal(inputconfig, 3)) )
+                    error('Input must be 0, 1, 2, or 3');
+                end
+                
+                
+                fprintf(obj.instr, 'ISRC %d', inputconfig);
+                
+            end
+            
+        end
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % shieldgrounding: Sets or reads the input shield to be        %
+        % floating (0) or grounded (1)                                 %
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+        function output = shieldgrounding(obj, shieldground)
+            if( nargin == 1 || isempty(shieldground) )
+                fprintf(obj.instr, 'IGND?');
+                output = fscanf(obj.instr, '%d');
+            else
+                
+                if( ~isnumeric(shieldground))
+                    error('Input Shield Grounding must be a number');
+                end
+                
+                if( ~(isequal(shieldground, 0) || isequal(shieldground, 1)) )
+                    error('Input must be 0 or 1');
+                end
+                
+                
+                fprintf(obj.instr, 'IGND %d', shieldground);
+                
+            end
+            
+        end
+        
         
         function output = iden(obj)
             output = query(obj.instr, '*IDN?');
